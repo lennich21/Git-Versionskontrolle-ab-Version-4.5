5c83a300b3fd5f82a784e83e08ba3c97f8dd0d74
Verleger, Jonas (mmjoverl) <jonas.verleger@stud.hs-ruhrwest.de>
2025-03-09 23:58:47 +0100

    Code aufgeräumt

--- EyeTrackerv6.py
+++ EyeTrackerv6.py
@@ -7,7 +7,7 @@ import carla
 import cv2
 import math
 import logging
-import pyautogui #kann Maus auslesen
+import pyautogui
 import csv
 
 class EyeTracker:
@@ -43,7 +43,7 @@ class Calibration:
         pass
 
     def calibration(self):
-        tracker = TrackerClient()
+        tracker = TrackerClient() # Import the API for the Beam Eye-Tracker
         
         user32 = ctypes.windll.user32   # Reads out current resolution
         screen_width = user32.GetSystemMetrics(0)
@@ -51,7 +51,7 @@ class Calibration:
         middle_width = screen_width/2
         middle_height = screen_height/2
 
-        calibration = tk.Tk()
+        calibration = tk.Tk() # Creates window for calibration
         calibration.title("Calibration")
         calibration.configure(bg="white")
 
@@ -62,7 +62,6 @@ class Calibration:
         calibration_window_y_position = int(middle_height) - int(calibration_window_y/2)
         calibration_window_geometry_string = str(calibration_window_x)+"x"+str(calibration_window_y)+"+"+str(calibration_window_x_position)+"+"+str(calibration_window_y_position)
         
-
         calibration.geometry(calibration_window_geometry_string)
         calibration.overrideredirect(True)
         
@@ -70,6 +69,7 @@ class Calibration:
         offset_x = round(calibration_window_x*0.1)
         offset_y = round(calibration_window_y*0.1)
 
+        # Creates button positions
         button_positions = [(offset_x, offset_y), (calibration_window_x - offset_x - 80, offset_y), (calibration_window_x - offset_x - 80, calibration_window_y - offset_y - 30), (offset_x, calibration_window_y - offset_y - 30), (round(calibration_window_x/2)-80/2, round(calibration_window_y/2)-30/2)]
         self.positions = [
             (offset_x+80/2, offset_y+30/2),
@@ -79,11 +79,10 @@ class Calibration:
             (middle_width, middle_height)
         ]
 
-        
-
         self.x = [0] * 5
         self.y = [0] * 5
 
+        # Creates button to save screen gaze data from Eye Tracker
         self.button = tk.Button(None, 
             text="Look Here", 
             font=("Helvetica", 11, "bold"), 
@@ -91,28 +90,29 @@ class Calibration:
             fg="white", 
             activebackground="green", 
             activeforeground="white", 
-            command=lambda: set_x_y(0)
+            command=lambda: set_xy(0)
         )
         self.button.place(x=button_positions[0][0], y=button_positions[0][1], width=80, height=30)
         
-
-        def set_x_y(i):
+        # Saves screen gaze data until all buttons have been pushed and terminates the calibration window
+        def set_xy(i):
             screen_gaze = tracker.get_screen_gaze_info()
             self.x[i], self.y[i] = screen_gaze.x, screen_gaze.y
             if i < 4:
                 self.button.place(x=button_positions[i+1][0], y=button_positions[i+1][1])
-                self.button.config(command=lambda: set_x_y(i+1))
+                self.button.config(command=lambda: set_xy(i+1))
             else:
                 calibration.destroy()
 
         if tracker.connected:
             calibration.mainloop()
-            self.calculate_optimization()
+            self.calculate_offset()
             return self.positions, self.x, self.y
         else:
             return "Tracker not connected"
         
-    def calculate_optimization(self):
+    # Calculates hard offset of Eye Tracker data and button position
+    def calculate_offset(self):
         
         difference_x_1 = self.x[0] - self.positions[0][0]
         difference_x_2 = self.x[1] - self.positions[1][0]
@@ -126,29 +126,21 @@ class Calibration:
         difference_y_4 = self.y[3] - self.positions[3][1]
         difference_y_5 = self.y[4] - self.positions[4][1]
 
-        print(
-            difference_x_1,
-            difference_x_2,
-            difference_x_3,
-            difference_x_4,
-            difference_x_5
-        )
+        # print(difference_x_1, difference_x_2, difference_x_3, difference_x_4, difference_x_5)
 
         self.average_difference_x = (difference_x_1 + difference_x_2 + difference_x_3 + difference_x_4 + difference_x_5) / 5
         self.average_difference_y = (difference_y_1 + difference_y_2 + difference_y_3 + difference_y_4 + difference_y_5) / 5
     
-
-    def optimize_pixels(self,TrackingData):
+    # Applies offset to Eye Tracker data
+    def add_offset(self,tracking_data):
         
-        Tracking_Data_list = list(TrackingData)
-        if(Tracking_Data_list[4]!="Null"):
-            Tracking_Data_list[4] = Tracking_Data_list[4] - round(self.average_difference_x)
+        if(tracking_data[4]!="Null"):
+            tracking_data[4] = tracking_data[4] - round(self.average_difference_x)
         
+        if(tracking_data[5]!="Null"):
+            tracking_data[5] = tracking_data[5] - round(self.average_difference_y)
         
-        if(Tracking_Data_list[5]!="Null"):
-            Tracking_Data_list[5] = Tracking_Data_list[5] - round(self.average_difference_y)
-        
-        return Tracking_Data_list
+        return tracking_data
     
 
 class CorrectCurvature:
@@ -209,7 +201,8 @@ class LookDirection:
             3: "Looks Right & Looks Down"
         }
 
-    def rough_look_direction(self, tracking_data): # Selects based on Eye-Tracker data, where one is looking
+    # Selects based on Eye-Tracker data, where one is looking
+    def look_direction_rough(self, tracking_data):
         if tracking_data[0] == "Tracker connected":
             if tracking_data[4] < self.screen_width/2 and tracking_data[5] < self.screen_height/2:
                 lookdir = self.lookdir.get(0, "Unknown")
@@ -221,16 +214,17 @@ class LookDirection:
                 lookdir = self.lookdir.get(3, "Unknown")
             return lookdir
 
-    def coordinates_look_direction(self, tracking_data, section_num): # Further specifies, where one is looking
+    # Further specifies, where one is looking
+    def look_direction_coordinates(self, tracking_data, section_amount):
         if tracking_data[0] == "Tracker connected":
-            section_num_x = int(section_num ** 0.5)
-            section_num_y = int(section_num ** 0.5)
-            section_width = self.screen_width / section_num_x
-            section_height = self.screen_height / section_num_y
+            section_amount_x = int(section_amount ** 0.5)
+            section_amount_y = int(section_amount ** 0.5)
+            section_width = self.screen_width / section_amount_x
+            section_height = self.screen_height / section_amount_y
             coordinate_x = int(tracking_data[4] // section_width)
             coordinate_y = int(tracking_data[5] // section_height)
-            coordinate_x = min(coordinate_x, section_num_x-1)
-            coordinate_y = min(coordinate_y, section_num_y-1)
+            coordinate_x = min(coordinate_x, section_amount_x-1)
+            coordinate_y = min(coordinate_y, section_amount_y-1)
             
             return coordinate_x, coordinate_y
 
@@ -239,7 +233,8 @@ class TextManager:
         self.canvas = tk.Canvas(root, width=700, height=400) # Creates a canvas inside the window
         self.canvas.pack()
 
-    def update_text(self, tracking_data, rough_look_direction, coordinates): # Creates text output to be displayed on the canvas inside the window
+    # Creates text output to be displayed on the canvas inside the window
+    def update_text(self, tracking_data, look_direction_rough, coordinates):
         if tracking_data[0] == "Tracker connected":
             self.text_content = [
                 "Beam Eye-Tracker connection state: " + tracking_data[0],
@@ -247,7 +242,7 @@ class TextManager:
                 "Confidence: " + str(tracking_data[2]),
                 "Coordinates: x = " + str(tracking_data[4]) + " px & y = " + str(tracking_data[5]) + " px",
                 "Confidence color: " + tracking_data[3],
-                "Looking direction: " + rough_look_direction,
+                "Looking direction: " + look_direction_rough,
                 "Screen quadrant: " + str(coordinates),
                 "Tag: " + tag,
                 "Instance ID: " + instance_id
@@ -255,10 +250,9 @@ class TextManager:
         else:
             self.text_content = ["Tracker not connected"]
             
-        # Everything gets deleted from canvas
         self.canvas.delete("all")
 
-        # Adds a text line based on text_content
+        # Adds a text line based on self.text_content
         y_position = 20  # Text starting location inside canvas
         for line in self.text_content:
             self.canvas.create_text(350, y_position, text=line, font=("Helvetica", 16), fill="black")
@@ -397,11 +391,11 @@ class CarlaClient:
 class csv_Logger:
     def __init__(self):
         timestamp = time.strftime("%Y%m%d_%H%M%S")
-        self.filename = f"log_{timestamp}.csv"
+        self.filename = f"Eye_Track_Log_{timestamp}.csv"
 
         with open(self.filename, mode="w", newline="") as file:
             writer = csv.writer(file)
-            writer.writerow(["Mauspos. x", "Mauspos. y", "Unkorrigiert x", "Unkorrigiert y", "Korrigiert x", "Korrigiert y"])
+            writer.writerow(["Cursor x", "Cursor y", "Beam Eye Pos. x", "Beam Eye Pos. y", "Adjusted x", "Adjusted y"])
 
     def log_data(self, data):
         with open(self.filename, mode="a", newline="") as file:
@@ -411,25 +405,25 @@ class csv_Logger:
 # Define inital value basic variables
 tag = "Null"
 instance_id = "Null"
-Tracking_Data = "Null", "Null", "Null", "Null", "Null", "Null"
-section_num = 16
+tracking_data = "Null", "Null", "Null", "Null", "Null", "Null"
+section_amount = 16
 
 # Runs calibration and returns pixel of the buttons and the tracker values
 #calibration = Calibration()
 #calibration_txt = calibration.calibration()
-#print("",calibration_txt)
 
 # Logging
-logging.basicConfig(filename="x_y_Track.txt", level=logging.INFO)
-
 logger = csv_Logger()
 
-# Build the window
+# Window to show canvas and black marker for eye position
+user32 = ctypes.windll.user32
+screen_width = user32.GetSystemMetrics(0)
+screen_height = user32.GetSystemMetrics(1)
 root = tk.Tk()
-root.title("Eye-Tracker")
-root.geometry("4444x1080+0+0")
+root.title("Eye Tracker")
+root.geometry(f"{screen_width}x{screen_height}+0+0")
 root.attributes('-alpha', 0.3)
-button2 = tk.Button(None, 
+marker = tk.Button(None, 
     text="", 
     font=("Helvetica", 11, "bold"), 
     bg="black", 
@@ -438,23 +432,22 @@ button2 = tk.Button(None,
     activeforeground="white", 
     )
 
-# Determine refresh rate of Eye-Tracker readout
+# Determine refresh rate of Eye Tracker readout
 start_time = time.time()
 frame_interval = 1/30 # 30 Hz refresh rate
 
-# Start reqiured classes
+# Start required classes
 eye_tracker = EyeTracker()
 look_direction = LookDirection()
 text_manager = TextManager()
 carla_client = CarlaClient()
 correct_curvature = CorrectCurvature()
 
-
 # Start CARLA simulation as client
 carla_client.connect_to_server("localhost")
 sensor = carla_client.example_situation()
 # Callback function to acquire instances of the segmentation
-sensor[0].listen(lambda image: carla_client.process_segmentation_image(image, Tracking_Data))
+sensor[0].listen(lambda image: carla_client.process_segmentation_image(image, tracking_data))
 
 # Callback function to acquire instances of the segmentation
 sensor[1].listen(lambda image: carla_client.process_rgb_image(image))
@@ -464,38 +457,33 @@ while True:
     if current_time - start_time >= frame_interval:
         start_time = current_time
 
-        x_m, y_m = pyautogui.position()
+        x_cur, y_cur = pyautogui.position() # Reads out cursor position -> Use it to know where you looked
 
-        Tracking_Data_Uncalibrated = eye_tracker.get_trackingdata()
-        x_1 = Tracking_Data_Uncalibrated[4]
-        y_1 = Tracking_Data_Uncalibrated[5]
-        # print("Unkalibriert: "+str(Tracking_Data_Uncalibrated))
-        # Curvate Correction
-        Tracking_Data_corrected_curvature = correct_curvature.correct(Tracking_Data_Uncalibrated)
-        # Calibration is deactivated for testing purposes of the curvature correction
-        #Tracking_Data = calibration.optimize_pixels(Tracking_Data_corrected_curvature)
+        tracking_data_unadjusted = eye_tracker.get_trackingdata()
+        x_1 = tracking_data_unadjusted[4]
+        y_1 = tracking_data_unadjusted[5]
+
+        # Calibration & Curvature Correction
+        tracking_data_adjusted = correct_curvature.correct(tracking_data_unadjusted)
+        #tracking_data_cal_adjusted = calibration.add_offset(tracking_data_adjusted) # Calibration is deactivated since curvature correction works better
 
         # For testing the original calibration is not done anymore. Only curvature calibration is done.
         # If calibration needs to be implemented again the curvature correction also needs to applied in line 96 where the calibration takes place.
         # Curvature Correciton must be done before screen calibration!!
-        Tracking_Data = Tracking_Data_corrected_curvature
-        x_2 = Tracking_Data[4]
-        y_2 = Tracking_Data[5]
-        # print("Kalibriert: "+str(Tracking_Data))
+        
+        tracking_data = tracking_data_adjusted
+        x_2 = tracking_data[4]
+        y_2 = tracking_data[5]
 
-        # Tracking_Data = eye_tracker.get_trackingdata()
-        Look_Direction = look_direction.rough_look_direction(Tracking_Data)
-        Look_Coordinates = look_direction.coordinates_look_direction(Tracking_Data, section_num)
-        text_manager.update_text(Tracking_Data, Look_Direction, Look_Coordinates)
+        look_direction = look_direction.look_direction_rough(tracking_data)
+        look_coordinates = look_direction.look_direction_coordinates(tracking_data, section_amount)
+        text_manager.update_text(tracking_data, look_direction, look_coordinates)
 
-        button2.place(x=Tracking_Data[4]-50/2, y=Tracking_Data[5]-50/2, width=50, height=50)
-        tk.Misc.lift(button2)
+        marker.place(x=tracking_data[4]-50/2, y=tracking_data[5]-50/2, width=50, height=50)
+        tk.Misc.lift(marker)
 
-        logging.info(f"Mausposition: ({x_m}, {y_m}), Unkalibriert: ({x_1}, {y_1}), Kalibriert: ({x_2}, {y_2})")
-        data = [x_m, y_m, x_1, y_1, x_2, y_2]
+        data = [x_cur, y_cur, x_1, y_1, x_2, y_2]
         logger.log_data(data)
 
         root.update()
         time.sleep(0.1)
-
-        
